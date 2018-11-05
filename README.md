6c4168c7ffb28101d7af0bddcdb19322fcc59a55
Deshun <admin@w3h5.com>
2018-11-05 08:44:00 +0800

    data-value
    data的取值和赋值

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # miniProgram-tmp
 我的微信小程序模板库（My WeChat miniProgram templates.）
+<ol>
+  <li>data 的取值和赋值</li>
+  <li>网址：</li>
+  <li>教程：</li>
+</ol>
---
+++ pages/data-value/data-value.js
@@ -0,0 +1,70 @@
+// pages/data-value/data-value.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    text: 'Hello world!',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      let that = this;
+      that.setData({
+        text: 'Happy Day!',
+      })
+      console.log(that.data.text);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
